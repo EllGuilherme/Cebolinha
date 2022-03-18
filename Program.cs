6ef193558b7b41548a2226209e10d197c6b9d4be
EllGuilherme <grds1000@gmail.com>
2022-03-18 20:11:58 -0300

    finalizar tarefa

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿String frasedigitada, frasecomL;
+
+Console.Write("digite uma frase: ");
+frasedigitada = Console.ReadLine()!;
+
+frasecomL = frasedigitada
+.Replace("r","l")
+.Replace("R", "L");
+
+Console.WriteLine(frasecomL);
+
